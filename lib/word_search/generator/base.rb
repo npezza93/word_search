ead6ab08a229a1db4b357900c67f8613d598c0ac
npezza93 <npezza93@gmail.com>
2016-12-25 11:46:11 -0500

    Raise not immplemented error for methods not in Generator base

--- lib/word_search/generator/base.rb
+++ lib/word_search/generator/base.rb
@@ -6,6 +6,10 @@ module WordSearch
       attr_accessor :plane, :word_bank, :used_coordinates
       delegate :to_s, :pto_s, :print, :catalog, to: :plane
 
+      def directions
+        raise NotImplementedError
+      end
+
       def initialize(plane, word_bank)
         @plane = plane
         @word_bank = word_bank
@@ -22,6 +26,18 @@ module WordSearch
 
       def random(number)
         SecureRandom.random_number number
+      end
+
+      def place_word
+        raise NotImplementedError
+      end
+
+      def position_word
+        raise NotImplementedError
+      end
+
+      def random_point
+        raise NotImplementedError
       end
     end
   end
